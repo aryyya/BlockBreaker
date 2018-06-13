2f5c789e6049d5b917e819f5dc7345928ee01f52
aryyya <arya@live.ca>
2018-06-13 03:21:27 -0700

    Add singleton music player.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    static MusicManager instance;
+
+    void Start()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
